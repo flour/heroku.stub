dffcfaff9daacb629fc8b57bd37be807cf8771d7
Vladislav Dukhnich <vladestt@gmail.com>
2019-11-27 14:51:28 +0300

    small animation update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Flipper, Flipped } from 'react-flip-toolkit';
 import logo from './logo.svg';
 import './App.css';
 
-const parseLocation = () => 
+const parseLocation = () =>
   window.location.search
     .substr(1)
     .split("&")
@@ -11,28 +11,45 @@ const parseLocation = () =>
 
 
 const App = () => {
-  const [data, setData] = React.useState([]);
+  const [items, setItems] = React.useState({ data: [], objs: parseLocation() });
   React.useEffect(() => {
-    setData(parseLocation());
-  }, [setData]);
+    if (!items.objs.length) {
+      return;
+    }
+    const timer = setTimeout(() => {
+      const newState = {
+        data: [...items.data, ...items.objs.splice(0, 1)],
+        objs: items.objs
+      };
+      setItems(newState);
+    }, 50);
+    return () => clearTimeout(timer);
+  }, [items, setItems]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <div className="paramsBlock">       
-        <div><b>Passed params:</b></div> 
-        <Flipper flipKey={`${data.map(item => item.param).join('_')}`}>         
-          
-            {data.map(item => (
-              <Flipped key={item.param} flipId={item.param} stagger>
-                <div className="list">
-                  <div className="param">{item.param}:</div>
-                  <div className=""><i>{item.value}</i></div>
-                </div>
-              </Flipped>
-            ))}    
+      <div className="paramsBlock">
+        <div><b>Passed params:</b></div>
+        <Flipper 
+          flipKey={`${items.data.map(item => item.param).join('_')}`}
+          staggerConfig={{
+            default: {
+              reverse: true,
+              speed: 1
+            }
+          }}
+        >
+          {items.data.map((item, i) => (
+            <Flipped key={`${item.param}-${i}`} flipId={item.param} stagger>
+              <div className="list">
+                <div className="param">{item.param}:</div>
+                <div className=""><i>{item.value}</i></div>
+              </div>
+            </Flipped>
+          ))}
         </Flipper>
       </div>
     </div>
